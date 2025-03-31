211bbb2f6b0b09942f7f8ace6944bd5dcdb7481c
msbevwilson <82551832+msbevwilson@users.noreply.github.com>
2025-03-30 22:23:55 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Housing from './components/Housing';
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+      <Router>
+      <header className="App-header">
+        <p>
+          header?
+        </p>
+      </header>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/about" element={ <About/> } />
+          <Route path="/housing" element={ <Housing/> } />
+        </Routes>
+      </Router>
+    </div>
+    </>
+  )
+}
---
+++ src/components/Housing.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Housing() {
+  return (
+    <div>
+      <h1>Housing</h1>
+      <p>Welcome to the Housing page!</p>
+    </div>
+  )
+}
