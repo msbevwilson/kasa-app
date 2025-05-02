5cbf0605bf22dbf24a837a75497b25c2f6e16c74
msbevwilson <82551832+msbevwilson@users.noreply.github.com>
2025-05-02 14:52:31 -0400

    wip

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import '../styles/404.scss';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function NotFound() {
 
     return (
-<div>
-<h1>Oops! We ran out of code</h1>
-</div>
+        <div className="not-found">
+            <div>
+                <Header />
+            </div>
+            <div className="not-found-body">
+                <div className="error-404">404</div>
+                <div className="oopsThePage">Oops! The page you’re looking for doesn’t exist.</div>
+                <div className="returnToHome">
+                    <a href="/">Return to home page</a>
+                </div>
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </div>
+        
     );
 }
